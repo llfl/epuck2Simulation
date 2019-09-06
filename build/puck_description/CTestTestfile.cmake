682a4f49871b487db3c90276b7a651018752eca7
Robot-first <jlhou8@gmail.com>
2019-09-06 21:47:49 +0800

    puck_rviz

---
+++ build/puck_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ernest/puck_ws/src/puck_description
+# Build directory: /home/ernest/puck_ws/build/puck_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
